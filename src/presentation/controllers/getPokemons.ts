78b407edcfebac776447ae954e9936568e3b844b
Michael <michaelhenriqued@gmail.com>
2022-01-22 14:12:06 -0300

    feat: ensure GetPokemonsController returns 400 if no offset is provided

--- src/presentation/controllers/getPokemons.ts
+++ src/presentation/controllers/getPokemons.ts
@@ -1,10 +1,17 @@
 export class GetPokemonsController {
   handle (httpRequest: any): any {
     const limit = JSON.parse(httpRequest.body).variables.limit
+    const offset = JSON.parse(httpRequest.body).variables.offset
     if (!limit) {
       return {
         statusCode: 400,
         body: new Error('Missing param: limit')
+      }
+    }
+    if (!offset) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: offset')
       }
     }
   }
