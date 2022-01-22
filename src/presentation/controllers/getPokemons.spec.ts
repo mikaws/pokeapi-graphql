b62348aec3716e67d4852588fc01676d43bd21a9
Michael <michaelhenriqued@gmail.com>
2022-01-22 14:12:25 -0300

    test: ensure GetPokemonsController returns 400 if no offset is provided

--- src/presentation/controllers/getPokemons.spec.ts
+++ src/presentation/controllers/getPokemons.spec.ts
@@ -14,4 +14,15 @@ describe('GetPokemons Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: limit'))
   })
+  test('Should return 400 if no offset is provided', () => {
+    const httpRequest = {
+      body: JSON.stringify({
+        query: 'any-query',
+        variables: { limit: 151 }
+      })
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: offset'))
+  })
 })
