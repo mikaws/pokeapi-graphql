75d05f16bc6e7660d52cabd888e74a8fa08dd9a4
Michael <michaelhenriqued@gmail.com>
2022-01-22 14:09:54 -0300

    test: ensure GetPokemonsController returns 400 if no limit is provided

---
+++ src/presentation/controllers/getPokemons.spec.ts
@@ -0,0 +1,17 @@
+import { GetPokemonsController } from './getPokemons'
+
+const sut = new GetPokemonsController()
+
+describe('GetPokemons Controller', () => {
+  test('Should return 400 if no limit is provided', () => {
+    const httpRequest = {
+      body: JSON.stringify({
+        query: 'any-query',
+        variables: { offset: 1 }
+      })
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: limit'))
+  })
+})
