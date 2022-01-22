055321e37b374b5dab185a194f7805c82f9f316c
Michael <michaelhenriqued@gmail.com>
2022-01-22 15:27:43 -0300

    feat: ensure getPokemonsController returns 400 if limit is less than zero

--- src/presentation/controllers/getPokemons.ts
+++ src/presentation/controllers/getPokemons.ts
@@ -1,15 +1,23 @@
 import { MissingParamError } from '../errors/missing-param-error'
+import { WrongParamError } from '../errors/wrong-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class GetPokemonsController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const { limit, offset } = JSON.parse(httpRequest.body).variables
-    if (!limit) {
+    const variables = JSON.parse(httpRequest.body).variables
+
+    // Missim Param Validators
+    if (!variables.limit) {
       return badRequest(new MissingParamError('limit'))
     }
-    if (!offset) {
+    if (!variables.offset && variables.offset !== 0) {
       return badRequest(new MissingParamError('offset'))
+    }
+
+    // Wrong Param Validators
+    if (variables.limit < 0) {
+      return badRequest(new WrongParamError('limit'))
     }
   }
 }
